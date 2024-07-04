280f85d1d9b4400ebec79ee676ec9403efa2419d
Pablo Alvarado <paalvarador@gmail.com>
2024-07-04 12:55:54 -0500

    feat: Cambiando la distribucion de la pagina para que se vea de mejor manera en distintos dispositivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,30 @@ import ContactForm from "@/components/ContactForm";
 
 export default function Home() {
   return (
-    <div>
-      <Header username="paalvarador" imageProfile="profile.jpg" />
-      <About
-        title="Acerca de mí"
-        description="Mi nombre es Pablo Alvarado, soy Ingeniero en Sistemas especializado en FullStack developer. Tengo un stack tecnológico bastante amplio el cual incluye tecnologías como HTML, CSS, JavaScript, Python, Node, React.js, Express.js, Node.js, Django, Flask, Laravel entre otros. Además tengo conocimiento en diseño e implementación de bases de datos como MySQL, Postrgresql, Oracle, entre otros"
-      />
-      <Interests
-        title="Intereses y Gustos"
-        interests={[
-          "Caminar por la ciudad",
-          "Conocer nuevos lugares",
-          "Disfrutar con mi familia el mayor tiempo posible",
-          "Tocar guitarra",
-          "Ver futbol",
-          "Hacer crossfit",
-          "Aprender Ingles",
-        ]}
-      />
-      <ContactForm />
+    <div className={styles.main}>
+      <div className={styles.profile_container_left}>
+        <Header username="paalvarador" imageProfile="profile.jpg" />
+        <Interests
+          title="Intereses y Gustos"
+          interests={[
+            "Caminar por la ciudad",
+            "Conocer nuevos lugares",
+            "Disfrutar con mi familia el mayor tiempo posible",
+            "Tocar guitarra",
+            "Ver futbol",
+            "Hacer crossfit",
+            "Aprender Ingles",
+          ]}
+        />
+      </div>
+      <div className={styles.profile_right}>
+        <About
+          title="Acerca de mí"
+          description="Mi nombre es Pablo Alvarado, soy Ingeniero en Sistemas especializado en FullStack developer. Tengo un stack tecnológico bastante amplio el cual incluye tecnologías como HTML, CSS, JavaScript, Python, Node, React.js, Express.js, Node.js, Django, Flask, Laravel entre otros. Además tengo conocimiento en diseño e implementación de bases de datos como MySQL, Postrgresql, Oracle, entre otros"
+        />
+
+        <ContactForm />
+      </div>
     </div>
   );
 }
