e48474b8fc51379f7134561b9a87ea8b43488510
Pablo Alvarado <paalvarador@gmail.com>
2024-11-10 19:08:45 -0500

    feat: Adding Google Analytics GTag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-60CQNL4W1C`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.datalayer = window.datalayer || [];
+              function gtag(){datalayer.push(arguments);}
+              gtag('js', new Data());
+              gtag('config', 'G-60CQNL4W1C', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
